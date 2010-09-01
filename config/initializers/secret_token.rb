1df823583a6404280b57500556a4cec53a1ae46e
Nick Ryberg <nryberg@gmail.com>
2010-08-31 22:15:58 -0500

    Second commit with bulk of rails content
    Raw rails project with the basics for the Toast-Schedule

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToastSchedule::Application.config.secret_token = '57188d1c8b8ddf52cc189c16f30ea9f8253542c8062d7811c18deb83a6f5d820843fa965f6fd15bd5c07824072e5ada295928525be0a39bacbf16c8a116f024e'
