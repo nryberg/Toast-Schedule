74bbc60bb481533fae6b2713a19548003b6cb5b0
Nick Ryberg <nryberg@gmail.com>
2010-09-13 22:12:34 -0500

    Still working on simple authentication.
    Stuck in a loop with a kEnd.  Will fix next.  Don't do these willy-nilly.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate(params[:name], params[:password])
       session[:user_id] = user.id
-      redirect_to admin_url
+      redirect_to clubs_url 
     else
       redirect_to login_url, :alert => "Invalid user/password combination"
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,13 @@ require 'digest/sha2'
 class User
   include MongoMapper::Document        
   
+  key :name, String, :unique => true, :presence => true
 
-#  key :email, String, :unique => true
-  key :email
+  key :email, String
   key :hashed_password, String
   key :salt, String
   key :confirmation_token, String
   key :remember_token, String
-  key :name, String, :unique => true, :presence => true
   key :email_confirmed, Boolean
  
     
@@ -21,22 +20,22 @@ class User
   attr_accessor :password_confirmation
   attr_reader :password
   
-  valid :password_must_be_present
+  validate :password_must_be_present
   
-    class << self
-      def authenticate(name, password)
-	if user = find_by_name(name)
-	  if user.hashed_password == encrypt_password(password, user.salt)
-	    user
-	  end
-	end
-      end
-
-      def encrypt_password(password, salt)
-	Digest::SHA2.hexdigest(password + "wibble" + salt)
+  class << self
+    def authenticate(name, password)
+      if user = find_by_name(name)
+        if user.hashed_password == encrypt_password(password, user.salt)
+          user
+        end
       end
     end
-  
+
+    def encrypt_password(password, salt)
+      Digest::SHA2.hexdigest(password + "washer" + salt)
+    end
+  end
+
   def password=(password)
     @password = password
     if password.present?
