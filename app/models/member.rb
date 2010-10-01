46542eb57be3d358b32d4983b1124eb66dd5ae1a
Nick <nick@nick-desktop.(none)>
2010-10-01 18:19:13 -0500

    Simplified model to just clubs, members and schedules.
    Will work on tough issues like "last one" leaves a light on - maybe a designation process for moving people out of roles?
    Change in leadership will always be difficult, but it should be easy to add a member or designate them as currently active in a role.

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,10 +7,20 @@ class Member
   key :club_id, ObjectId
   key :email, String
   key :phone, String
+  key :can_edit, Boolean
+  key :role, String
+ 
+  validates_presence_of :name
+  validate :password, :confirmation => true  
+  attr_accessor :password_confirmation  
+  attr_reader :password  
+    
+  validate :password_must_be_present  
+  
  
 # Validations :::::::::::::::::::::::::::::::::::::::::::::::::::::
 # validates_presence_of :attribute
-
+ 
 # Assocations :::::::::::::::::::::::::::::::::::::::::::::::::::::
   belongs_to :club
 # many :model
@@ -32,6 +42,36 @@ class Member
 
 # Typecast
 # key :user_ids, Array, :typecast => 'ObjectId'
+  class << self
+    def authenticate(email, password)  
+      if user = find_by_name(email)  
+        if user.hashed_password == encrypt_password(password, user.salt)  
+          user  
+        end  
+      end  
+    end  
+  
+    def encrypt_password(password, salt)  
+      Digest::SHA2.hexdigest(password + "washer" + salt)  
+    end  
+  end  
+  
+  def password=(password)  
+    @password = password  
+    if password.present?  
+      generate_salt  
+      self.hashed_password = self.class.encrypt_password(password, salt)  
+    end  
+  end  
   
    
-end
+  private  
+    def password_must_be_present  
+      errors.add(:password, "Missing password" ) unless hashed_password.present?  
+    end  
+      
+    def generate_salt  
+      self.salt = self.object_id.to_s + rand.to_s  
+    end  
+   
+end                                                 
