10adfd342bdd8bea41444753c1e24335249ab164
Nick <nick@laptopper.(none)>
2011-04-27 22:58:38 -0500

    Switched back to MongoHQ

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -2,14 +2,14 @@
 MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
 # MongoMapper.database = "ts-#{Rails.env}"
 
+MongoMapper.database = 'toast_schedule'
 # Mongo HQ ###
-# MongoMapper.connection = Mongo::Connection.new('flame.mongohq.com', 27038)
-# MongoMapper.database.authenticate('toast_user', 'gidwysEzlaug')
+MongoMapper.connection = Mongo::Connection.new('flame.mongohq.com', 27038)
+MongoMapper.database.authenticate('toast_user', 'gidwysEzlaug')
 
 # Shared Drive ###
 # MongoMapper.connection = Mongo::Connection.new('192.168.0.100', 27017)
 
-MongoMapper.database = 'toast_schedule'
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|
