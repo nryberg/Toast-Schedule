fd1d20b00f188df14da23a4fad6f8b78835ea24d
Nick <nick@macbook.(none)>
2011-07-27 22:53:20 -0500

    Updated Mongodb

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,11 +1,11 @@
 # Local ###
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+# MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
 # MongoMapper.database = "ts-#{Rails.env}"
 
 MongoMapper.database = 'toast_schedule'
 # Mongo HQ ###
-# MongoMapper.connection = Mongo::Connection.new('flame.mongohq.com', 27038)
-# MongoMapper.database.authenticate('toast_user', 'gidwysEzlaug')
+MongoMapper.connection = Mongo::Connection.new('flame.mongohq.com', 27038)
+MongoMapper.database.authenticate('toast_user', 'gidwysEzlaug')
 
 # Shared Drive ###
 # MongoMapper.connection = Mongo::Connection.new('192.168.0.100', 27017)
