376cd1660dc42f7e08fecea252e9b47e0f3af46a
Nick <nick@macbook.(none)>
2011-08-16 20:28:24 -0500

    Adding back SQL Lite and trying to get mongomapper to work

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,12 +1,13 @@
 # Local ###
 if ENV['RAILS_ENV'] == "development" 
   MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+  MongoMapper.database = 'toast_schedule'
 elsif ENV['RAILS_ENV'] == "production" 
   MongoMapper.connection = Mongo::Connection.new('flame.mongohq.com', 27038)
   MongoMapper.database.authenticate('toast_user', 'gidwysEzlaug')
+  MongoMapper.database = 'toast_schedule'
 end
 
-MongoMapper.database = 'toast_schedule'
 # Mongo HQ ###
 
 # Shared Drive ###
