af5d21ee8f68024d2b36d1eb05ad088e3c048f82
Nick <nick@macbook.(none)>
2011-09-19 23:11:26 -0500

    Sigh.  Not liking complex forms max.

--- app/helpers/templates_helper.rb
+++ app/helpers/templates_helper.rb
@@ -1,2 +1,13 @@
 module TemplatesHelper
+  def add_role_link(name) 
+    #link_to_function name,  "$j('.plates').append('= render(:partial => @plate), :object => Plate.new' );" 
+    link_to_function name,  "$j('.plates').append('!= render(@plate)' );" 
+
+    #  link_to_function name, "$j('.plates').css('border','1px solid #993300');"
+      # "page.insert_html :bottom, :plates, :partial => 'plate', :object => Plate.new" 
+
+
+#      "remove", "var elem = this; $j(elem).parent().remove()"
+  end
+
 end
