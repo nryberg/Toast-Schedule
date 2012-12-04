808fea4801f6ea9acedc58932944b34ab56772c2
Nick Ryberg <nryberg@gmail.com>
2012-12-03 18:48:17 -0600

    Completes password reset functionality.
    Next steps - style the reset path to be consistent

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -7,8 +7,22 @@ class PasswordResetsController < ApplicationController
   end
 
 
-
+  def edit
+    @member = Member.find_by_password_reset_token!(params[:id])
+  end
 
   def new
+  end
+
+  def update
+    @member = Member.find_by_password_reset_token!(params[:id])
+    if @member.password_reset_sent_at < 2.hours.ago
+      redirect_to new_password_reset_path, :alert => "Password &crarr; 
+        reset has expired."
+    elsif @member.update_attributes(params[:member])
+      redirect_to root_url, :notice => "Password has been reset."
+    else
+      render :edit
+    end
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,13 @@ ToastSchedule::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # Mail config
+  # TODO: Change the host to TMSchedule for production after testing is done
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 end
