a2b03bcdb801ce13925705537c9d137d06594931
Nick Ryberg <nryberg@gmail.com>
2013-02-16 12:32:05 -0600

    Moves dev to actually sending email for testing

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,5 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.mandrillapp.com",
-  :port                 => 587,
-  :domain               => "tmschedule.me",
-  :user_name            => "info@tmschedule.me",
-  :password             => "cbaf3e82-5279-4448-9a8e-03f4716dffd8",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+if Rails.env != 'production'
+  email_settings = YAML::load(File.open("#{Rails.root.to_s}/config/email.yml"))
+  ActionMailer::Base.smtp_settings = email_settings[Rails.env] unless email_settings[Rails.env].nil?
+end
+
