526e480d6e94dc07872c1ad6bd3c54dd5b78b280
Nick Ryberg <nryberg@gmail.com>
2013-02-18 16:29:47 -0600

    Starts new mailer templates, cleanup TODO

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -17,33 +17,6 @@ class MembersController < ApplicationController
     end
   end
 
-  def search
-
-    #TODO This entire section needs to be taken out and shot.
-    #     Fix it.
-    #     Until then - go with the simplest possible model.  Go back to the original add member route and trap
-    #     the problems where they lie.
-    #     10:09 PM 02/12/2013
-
-    @params = params[:search]
-    current_club = Club.find(session[:club_id]) 
-    unless params[:search].nil?
-      @search_string = params[:search]
-      @candidate = Member.find_by_email(@search_string)
-      if @candidate
-        @candidate.memberships.each {|x| ap x}
-        #@membership = Membership.first(:member_id => @candidate.id, :club_id => session[:club_id])  ||= Membership.new
-        @membership.club = current_club
-        @membership.member = @candidate
-        redirect_to @membership
-      end if
-      
-      @existing = current_club.all_members
-      @members = @candidate - @existing
-    end
-
-  end
-
   def edit
     @member = Member.find(params[:id])
     #@club_choices = @member.clubs
@@ -76,27 +49,12 @@ class MembersController < ApplicationController
   end
   
   def create
-
-    # TODO : Build out duplication testing for member.  If they already exist, move the user
-    #        to a recovery where they can simply assign that person a new role.
-    #        This may cause problems with duplicates with similar but different names.
-    #         09:59 PM 11/14/2012
-
-    # 11:33 PM 12/04/2012
-    # This whole section needs to be cleaned up.  There's a lot of confusion going on here. 
-
-    # Forget worrying about whether or not the new member is on the books.
-    # If they don't exist, add them.
     # If they do exist, but aren't in the club, add the membership
     # If they do exist and have a membership, go to edit the membership.
     # At the end of the day, the result is the same.  
     #   1) Pull a member, build if it doesn't exist
     #   2) Pull the membership, build if it doesn't exist
     #   3) Move to the membership edit
-
-    # 08:09 PM 01/12/2013
-    # Getting down to brass tacks.  Keep it simple stupid.  Memberships will be in the background - can be guest, member or
-    # officer based on the timestamp for each value
 
     message = "(Blank)"
     email = params[:member][:email]
