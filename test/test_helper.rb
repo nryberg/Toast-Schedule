fe08ce73fd1b2b6d005e7eec01c02d196e1c7104
Nick Ryberg <nryberg@gmail.com>
2014-01-15 22:14:58 -0600

    Moving to rspec

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,9 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
  # Drop all collections after each test case.
- def teardown
+
+class ActiveSupport::TestCase
+  def teardown
    MongoMapper.database.collections.each do |coll|
      coll.remove
    end
@@ -10,18 +12,16 @@ require 'rails/test_help'
 
 # Make sure that each test case has a teardown
 # method to clear the db after each test.
-def inherited(base)
-  base.define_method :teardown do
-    super
+  def inherited(base)
+   base.define_method :teardown do
+     super
+   end
   end
-end
-
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+ # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
-  #fixtures :all
+  fixtures :users
 
   # Add more helper methods to be used by all tests here...
 end
